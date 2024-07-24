8e6da5f016e0e4b9d36b9fa15cad44d9e64eb181
bgallois <benjamin@gallois.cc>
2024-07-24 15:22:17 +0200

    [feat] add doc

--- app.py
+++ app.py
@@ -39,28 +39,6 @@ def index():
     return render_template('map.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-        coordinates = process_gpx(filepath)[0]
-        if not coordinates:
-            return jsonify({'error': 'Invalid GPX file'}), 400
-        data = {
-            "coordinates": coordinates
-        }
-        return jsonify(data)
-    else:
-        return jsonify({'error': 'File type not allowed'}), 400
-
-
 def process_gpx(file_path):
     with open(file_path, 'r') as gpx_file:
         gpx = gpxpy.parse(gpx_file)
